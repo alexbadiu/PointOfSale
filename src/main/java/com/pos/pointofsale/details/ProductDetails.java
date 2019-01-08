bc4b46c098cd75be9a1c09e4f439531ed8636829
Alex <alexbadiu26@yahoo.com>
2019-01-08 23:28:45 +0200

    Added Product entity, EJB and Product Details

---
+++ src/main/java/com/pos/pointofsale/details/ProductDetails.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pos.pointofsale.details;
+
+/**
+ *
+ * @author Alex
+ */
+public class ProductDetails implements java.io.Serializable {
+    
+    private Integer id;
+    
+    private String prodName;
+
+    public ProductDetails(Integer id, String prodName) {
+        this.id = id;
+        this.prodName = prodName;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getProdName() {
+        return prodName;
+    }
+
+    public void setProdName(String prodName) {
+        this.prodName = prodName;
+    }
+}
---
+++ src/main/java/com/pos/pointofsale/entity/Product.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pos.pointofsale.entity;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Alex
+ */
+@Entity
+@Table(name = "PRODUCTS")
+public class Product implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    
+    private Integer id;
+    
+    private String prodName;
+    
+    private ProductSpecification productSpecification;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getProdName() {
+        return prodName;
+    }
+
+    public void setProdName(String prodName) {
+        this.prodName = prodName;
+    }
+
+    @OneToOne(mappedBy = "product")
+    public ProductSpecification getProductSpecification() {
+        return productSpecification;
+    }
+
+    public void setProductSpecification(ProductSpecification productSpecification) {
+        this.productSpecification = productSpecification;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Product)) {
+            return false;
+        }
+        Product other = (Product) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.pos.pointofsale.entity.Product[ id=" + id + " ]";
+    }
+    
+}
