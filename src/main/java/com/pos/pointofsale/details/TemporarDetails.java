810f62009eace1db058d93672a2a681b6bf25c1d
Alex <alexbadiu26@yahoo.com>
2019-01-09 00:15:46 +0200

    Added Temporar entity, EJB and TemporarDetails

---
+++ src/main/java/com/pos/pointofsale/details/TemporarDetails.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pos.pointofsale.details;
+
+/**
+ *
+ * @author Asus
+ */
+public class TemporarDetails implements java.io.Serializable {
+    
+    private Integer id;
+    
+    private String name;
+    private String description;
+    private Double price;
+    private Integer quantity;
+
+    public TemporarDetails(Integer id, String name, String description, Double price, Integer quantity) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+    
+}
